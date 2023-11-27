5aa7b06ba7f5aa226dd043bb79beb47cf7d13e8e
Vitor Manoel do Santos <vitormanoel3000@gmail.com>
2023-11-27 09:28:37 -0300

    Deixando o jogo mais fácil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela (tag, texto){
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Adivinhe o numero secreto');
-    exibirTextoNaTela('p', 'Escolha entre um numero de 1 a 100');
+    exibirTextoNaTela('p', 'Escolha entre um numero de 1 a 50');
 }
 
 exibirMensagemInicial()
